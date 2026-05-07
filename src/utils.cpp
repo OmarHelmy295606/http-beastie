4300f7a53599ed985b0ddfff5820dd152acb1b9d
OmarHelmy295606 <omarhelmy295@aucegypt.edu>
2026-05-07 17:09:45 +0300

    Updated the handle_request function to retuns the actual html file content

--- src/utils.cpp
+++ src/utils.cpp
@@ -17,7 +17,7 @@ http::response<http::string_body> handle_request(const http::request<http::strin
   if (req.method() == http::verb::get) {
     if (req.target() == "/") {
       res.result(http::status::ok);
-      res.body() = "<h1 style=\"text-align: center;\">CSCE 1102</h1>";
+      res.body() = read_file("static/index.html");
     } else {
       res.result(http::status::not_found);
       res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
@@ -34,7 +34,7 @@ http::response<http::string_body> handle_request(const http::request<http::strin
 
 std::string read_file(const std::string& path){
 	std::ifstream file(path);
-	if(!file.isOpen())
+	if(!file.is_open())
 		return "";
 
 	std::ostringstream ss;
